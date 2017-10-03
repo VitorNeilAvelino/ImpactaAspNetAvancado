b971f07c60cf776f120ee2e3ba44cbb406ec7691
Vítor Avelino <avelino.vitor@gmail.com>
2017-10-02 21:33:44 -0300

    Área com subpastas

---
+++ Loja.Mvc/Areas/Admin/Controllers/Produtos/ProdutosController.cs
@@ -0,0 +1,15 @@
+﻿using Loja.Mvc.Areas.Vendas.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Loja.Mvc.Areas.Admin.Controllers.Produtos
+{
+    public class ProdutosController : Controller
+    {
+        // GET: Admin/Produtos
+        public ActionResult Index()
+        {
+            return View(new List<ProdutoViewModel> { new ProdutoViewModel() });
+        }
+    }
+}
---
+++ Loja.Mvc/Areas/Admin/Controllers/Usuarios/UsuariosController.cs
@@ -0,0 +1,18 @@
+﻿using Loja.Mvc.Areas.Vendas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loja.Mvc.Areas.Admin.Controllers.Usuarios
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Admin/Usuarios
+        public ActionResult Index()
+        {
+            return View(new List<ProdutoViewModel> { new ProdutoViewModel() });
+        }
+    }
+}
