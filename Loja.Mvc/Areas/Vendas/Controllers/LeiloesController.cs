b1818e551afc380a5883f82b8eac5c1ee2495be4
Vítor Avelino <avelino.vitor@gmail.com>
2017-10-13 20:22:41 -0300

    LeiloesController

---
+++ Loja.Mvc/Areas/Vendas/Controllers/LeiloesController.cs
@@ -0,0 +1,17 @@
+﻿using Loja.Mvc.Helpers;
+using Loja.Repositorios.SqlServer.EF;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Loja.Mvc.Areas.Vendas.Controllers
+{
+    public class LeiloesController : Controller
+    {
+        private readonly LojaDbContext _db = new LojaDbContext();
+
+        public ActionResult Index()
+        {
+            return View(Mapeamento.Mapear(_db.Produtos.Where(p => p.EmLeilao).ToList()));
+        }
+    }
+}
