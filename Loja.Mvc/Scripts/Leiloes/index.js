b484b83b3ab2025f740b2da85828881cfae97b62
Vítor Avelino <avelino.vitor@gmail.com>
2017-11-07 23:05:45 -0200

    Index.js

---
+++ Loja.Mvc/Scripts/Leiloes/index.js
@@ -0,0 +1,19 @@
+﻿var Index = {
+    inicializar: function () {
+        this.conectarLeilaoHub();
+    },
+
+    conectarLeilaoHub: function(){
+        var connection = $.hubConnection();
+        var hub = connection.createHubProxy("LeilaoHub");
+
+        //"ATUALIZARLISTA": cusiosamente, não é case sensitive.
+        hub.on("atualizarOfertas", this.atualizarOfertas.bind(this));
+
+        connection.start();
+    },
+
+    atualizarOfertas: function(){
+        document.location.reload();
+    }
+};
