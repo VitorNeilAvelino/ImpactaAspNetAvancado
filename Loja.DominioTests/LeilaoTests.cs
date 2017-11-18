6fe6b47021c667ebdf65fc1a7ff9238138bec7ad
Vítor Avelino <avelino.vitor@gmail.com>
2017-11-17 22:59:38 -0200

    LeilaoTeste

---
+++ Loja.DominioTests/LeilaoTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Loja.Dominio.Tests
+{
+    [TestClass()]
+    public class LeilaoTests
+    {
+        [TestMethod()]
+        public void ValidarSucessoTest()
+        {
+            //Arranja
+            var leilao = new Leilao();
+            leilao.NomeLote = "NomeLote";
+            leilao.Valor = 90;
+            leilao.Produtos = new List<Produto> { new Produto { Preco = 100 } };
+
+            //Age
+            var erros = leilao.Validar();
+
+            //Afirma
+            Assert.IsTrue(erros.Count == 0);
+        }
+
+        [TestMethod]
+        public void ValidarErroDescontoTeste()
+        {
+            //Arranja
+            var leilao = new Leilao();
+            leilao.NomeLote = "NomeLote";
+            leilao.Valor = 80;
+            leilao.Produtos = new List<Produto> { new Produto { Preco = 100 } };
+
+            //Age
+            var erros = leilao.Validar();
+
+            //Afirma
+            Assert.IsTrue(erros.Contains("Desconto máximo excedido."));
+        }
+    }
+}
